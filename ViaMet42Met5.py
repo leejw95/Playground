b7ae00eb1f80f697f8fb2acda2434cd0e6bafe16
IksuJang <isjang@postech.ac.kr>
2021-10-28 14:06:43 +0900

    Edit PmosSet for CML Driver

--- ViaMet42Met5.py
+++ ViaMet42Met5.py
@@ -254,5 +254,60 @@ class _ViaMet42Met5(StickDiagram._StickDiagram):
         print ('                                    {}  ViaMet42Met5 Calculation End                                    '.format(self._DesignParameter['_Name']['_Name']))
         print ('#########################################################################################################')
 
+
+    def _CalculateDesignParameterSameEnclosure(self, _ViaMet42Met5NumberOfCOX=None, _ViaMet42Met5NumberOfCOY=None):
+        print ('#########################################################################################################')
+        print ('                                    {}  ViaMet42Met5 Calculation Start                                    '.format(self._DesignParameter['_Name']['_Name']))
+        print ('#########################################################################################################')
+
+        ###############################################Check the number of CO On Via Contact###########################################################################################
+        if _ViaMet42Met5NumberOfCOX == 0 or _ViaMet42Met5NumberOfCOY == 0:
+            print ('************************* Error occured in {} Design Parameter Calculation******************************'.format(self._DesignParameter['_Name']['_Name']))
+            if DesignParameters._DebugMode == 0:
+                return 0
+        ###############################################################################################################################################################################
+        _DRCObj = DRC.DRC()
+        _XYCoordinateOfViaMet42Met5 = [[0,0]]
+
+        print ('#############################     Met5 Layer Calculation   ##############################################')
+        _LengthViaMet42Met5BtwCO = _DRCObj._VIAxMinWidth + _DRCObj.DRCVIAxMinSpace(NumOfVIAxX=_ViaMet42Met5NumberOfCOX,NumOfVIAxY=_ViaMet42Met5NumberOfCOY)
+        self._DesignParameter['_Met5Layer']['_XYCoordinates'] = _XYCoordinateOfViaMet42Met5
+        self._DesignParameter['_Met5Layer']['_XWidth'] = _DRCObj._VIAxMinWidth + (_ViaMet42Met5NumberOfCOX - 1) * _LengthViaMet42Met5BtwCO + 2 * max([_DRCObj._Metal1MinEnclosureVia3,_DRCObj._MetalxMinEnclosureVia3])
+        self._DesignParameter['_Met5Layer']['_YWidth'] = _DRCObj._VIAxMinWidth + (_ViaMet42Met5NumberOfCOY - 1) * _LengthViaMet42Met5BtwCO + 2 * max([_DRCObj._Metal1MinEnclosureVia3,_DRCObj._MetalxMinEnclosureVia3])
+
+        print ('#############################     Met2 Layer Calculation   ##############################################')
+        _LengthViaMet42Met5BtwCO = _DRCObj._VIAxMinWidth + _DRCObj.DRCVIAxMinSpace(NumOfVIAxX=_ViaMet42Met5NumberOfCOX,NumOfVIAxY=_ViaMet42Met5NumberOfCOY)
+        self._DesignParameter['_Met4Layer']['_XYCoordinates'] = _XYCoordinateOfViaMet42Met5
+        self._DesignParameter['_Met4Layer']['_XWidth'] = _DRCObj._VIAxMinWidth + (_ViaMet42Met5NumberOfCOX - 1) * _LengthViaMet42Met5BtwCO + 2 * max([_DRCObj._Metal1MinEnclosureVia3,_DRCObj._MetalxMinEnclosureVia3])
+        self._DesignParameter['_Met4Layer']['_YWidth'] = _DRCObj._VIAxMinWidth + (_ViaMet42Met5NumberOfCOY - 1) * _LengthViaMet42Met5BtwCO + 2 * max([_DRCObj._Metal1MinEnclosureVia3,_DRCObj._MetalxMinEnclosureVia3])
+
+        print ('#############################     Cont Layer Calculation   ##############################################')
+        tmp = []
+        self._DesignParameter['_COLayer']['_XWidth'] = _DRCObj._VIAxMinWidth
+        self._DesignParameter['_COLayer']['_YWidth'] = _DRCObj._VIAxMinWidth
+        _LengthViaMet42Met5BtwCO = _DRCObj._VIAxMinWidth + _DRCObj.DRCVIAxMinSpace(NumOfVIAxX=_ViaMet42Met5NumberOfCOX,NumOfVIAxY=_ViaMet42Met5NumberOfCOY)
+        for i in range(0, _ViaMet42Met5NumberOfCOX):
+            for j in range(0, _ViaMet42Met5NumberOfCOY):
+                if (_ViaMet42Met5NumberOfCOX % 2) == 0 and (_ViaMet42Met5NumberOfCOY % 2)==0:
+                    _xycoordinatetmp = [_XYCoordinateOfViaMet42Met5[0][0] - (_ViaMet42Met5NumberOfCOX / 2 - 0.5) * _LengthViaMet42Met5BtwCO + i * _LengthViaMet42Met5BtwCO,
+                                        _XYCoordinateOfViaMet42Met5[0][1] - (_ViaMet42Met5NumberOfCOY / 2 - 0.5 )*_LengthViaMet42Met5BtwCO + j*_LengthViaMet42Met5BtwCO]
+                elif (_ViaMet42Met5NumberOfCOX % 2) == 0 and (_ViaMet42Met5NumberOfCOY % 2)==1:
+                    _xycoordinatetmp = [_XYCoordinateOfViaMet42Met5[0][0] - (_ViaMet42Met5NumberOfCOX / 2 - 0.5) * _LengthViaMet42Met5BtwCO + i * _LengthViaMet42Met5BtwCO,
+                                        _XYCoordinateOfViaMet42Met5[0][1] - (_ViaMet42Met5NumberOfCOY-1)/2 * _LengthViaMet42Met5BtwCO +j*_LengthViaMet42Met5BtwCO]
+                elif (_ViaMet42Met5NumberOfCOX % 2) == 1 and (_ViaMet42Met5NumberOfCOY % 2)==0:
+                    _xycoordinatetmp = [_XYCoordinateOfViaMet42Met5[0][0] - (_ViaMet42Met5NumberOfCOX -1) / 2  * _LengthViaMet42Met5BtwCO + i * _LengthViaMet42Met5BtwCO,
+                                        _XYCoordinateOfViaMet42Met5[0][1] - (_ViaMet42Met5NumberOfCOY / 2 - 0.5 )*_LengthViaMet42Met5BtwCO + j*_LengthViaMet42Met5BtwCO]
+                elif (_ViaMet42Met5NumberOfCOX % 2) == 1 and (_ViaMet42Met5NumberOfCOY % 2)==1:
+                    _xycoordinatetmp = [_XYCoordinateOfViaMet42Met5[0][0] - (_ViaMet42Met5NumberOfCOX -1) / 2 * _LengthViaMet42Met5BtwCO + i * _LengthViaMet42Met5BtwCO,
+                                        _XYCoordinateOfViaMet42Met5[0][1] - (_ViaMet42Met5NumberOfCOY-1)/2 * _LengthViaMet42Met5BtwCO +j*_LengthViaMet42Met5BtwCO]
+                tmp.append(_xycoordinatetmp)
+        self._DesignParameter['_COLayer']['_XYCoordinates'] = tmp
+
+        print ('#########################################################################################################')
+        print ('                                    {}  ViaMet42Met5 Calculation End                                    '.format(self._DesignParameter['_Name']['_Name']))
+        print ('#########################################################################################################')
+
+
+
 if __name__=='__main__':
     pass

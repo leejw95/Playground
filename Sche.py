8730fd2f94a37cecbb2b968d5eb0b8eaa25cd20b
junung0110 <junung@postech.ac.kr>
2022-06-01 18:11:30 +0900

    sche making

--- Sche.py
+++ Sche.py
@@ -1,8 +1,19 @@
 import re
 import sys
+import os
 import DesignParameters
 
 
 class Schematic () :
-    def __init__ (self, params):
-        pass
+    def __init__ (self, parameter, arch):
+        self.parameter = parameter
+        self.arch = arch
+
+    def SchematicGenerator (self) :
+        _HomeDirectory = os.getcwd()
+        sch = open(_HomeDirectory + "/D_FF_test/{}.src.net".format(self.arch), 'r')
+        lines = sch.readlines()
+        for line in lines : 
+            print (line)
+        sch.close()
+
